f89a62e2e2053f593286e5f8215cbacaa63332e0
K0lumna <michaljaniec12@wp.pl>
2022-08-09 20:40:18 +0200

    class include

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,38 @@
 const odpowiedzi = [];
+let zycia = 3;
+let punkty = 0;
+
+class Odpowiedz {
+    constructor(odp, pkt) {
+        this._odp = odp;
+        this._pkt = pkt;
+    }
+
+    get odp() {
+        return this._odp;
+    }
+
+    get pkt() {
+        return this._pkt;
+    }
+
+}
 
 const getOdpowiedzi = () => {
 
     for(let i = 0; i <= 4; i++) {
-        odpowiedzi.push(document.querySelector(`#i${i}`).value);
+        odpowiedzi.push(new Odpowiedz(document.querySelector(`#i${i}`).value, document.querySelector(`#p${i}`).value));
     }
 
     if(odpowiedzi.length === 5) {
         for(let i = 0; i <= 4; i++) {
             document.querySelector(`#i${i}`).value = null;
+            document.querySelector(`#p${i}`).value = null;
         }
     }
 
-    console.log(odpowiedzi);
+    console.log(odpowiedzi[4].odp);
+
 }
 
 const getWynik = () => {
@@ -25,9 +45,7 @@ const getWynik = () => {
         }
     });
 
-    if(check) {
-        console.log(`${wynik} jest w tablicy`);
-    } else {
-        console.log(`${wynik} nie jest w tablicy...`);
+    for (i in odpowiedzi) {
+        document.querySelector('#wyniki').innerHTML += `<tr><td>${odpowiedzi[i].odp}</td><td>${odpowiedzi[i].pkt}pkt</td><td>${zycia}</td></tr>`;
     }
 }
