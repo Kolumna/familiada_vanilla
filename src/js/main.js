23f095adf70d020c33245fa31aa82a40b2000077
K0lumna <michaljaniec12@wp.pl>
2022-08-10 22:37:37 +0200

    added more systems

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,18 @@ const odpowiedzi = [];
 let zyciaL = 3;
 let zyciaP = 3;
 let punkty = 0;
-let strona =  Math.floor(Math.random() * Math.floor(2));
+let strona = null;
+let rewanz = null;
+let wersy = 0;
 
-if (strona === 0) {
-    document.querySelector('#tlo').style = 'background: #FF6C6C;';
-} else {
-    document.querySelector('#tlo').style = 'background: #6CC5FF;';
+const colorMaker = () => {
+    if (strona === 0) {
+        document.querySelector('#tlo').style = 'background: #6CC5FF;';
+    } else {
+        document.querySelector('#tlo').style = 'background: #FF6C6C;';
+    }
 }
+
 
 document.querySelector('#strona').innerHTML = strona;
 
@@ -29,6 +34,24 @@ class Odpowiedz {
 
 }
 
+const getStart = () => {
+    document.querySelector('#gra').style = 'display: flex;';
+    document.querySelector('#form').style = 'display: none;';
+    document.querySelector('#checkbox').style = 'display: none;';
+
+    if (document.querySelector('#nieb').checked === true) {
+        console.log('nieb')
+        strona = 0;
+        rewanz = 1;
+    } else {
+        console.log('czer')
+        strona = 1;
+        rewanz = 0;
+    }
+
+    colorMaker();
+}
+
 const getOdpowiedzi = () => {
 
     for(let i = 0; i <= 4; i++) {
@@ -41,8 +64,8 @@ const getOdpowiedzi = () => {
             document.querySelector(`#p${i}`).value = null;
         }
 
-        document.querySelector('#gra').style = 'display: flex;'
-        document.querySelector('#form').style = 'display: none;'
+        document.querySelector('#checkbox').style = 'display: flex;';
+        document.querySelector('#form').style = 'display: none;';
     }
 
     for (i in odpowiedzi) {
@@ -54,56 +77,80 @@ const getWynik = () => {
     const wynik = document.querySelector(`#wynik`).value;
     let check = false;
 
-    if (zyciaL && zyciaP > 0) {
+    if (zyciaL || zyciaP > 0) {
         for (let i in odpowiedzi) {
             if (wynik === odpowiedzi[i].odp) {
                 document.querySelector(`#w${i}`).style = '';
                 check = true;
+                wersy += 1;
+                if (wersy === 5 && strona === 0) {
+                    document.querySelector('#results').innerHTML = '<h1>Niebieski wygrał</h1>';
+                    document.querySelector('#gra').style = 'display: none;';
+                } else if (wersy === 5 && strona === 1) {
+                    document.querySelector('#results').innerHTML = '<h1>Czerwony wygrał</h1>';
+                    document.querySelector('#gra').style = 'display: none;';
+                }
             }
         }
 
         if (!check) {
             if (strona === 0) {
                 zyciaL -= 1;
+                document.querySelector('#strona').innerHTML = strona;
+                if (rewanz === 1) {
+                    switch (zyciaL) {
+                        case 2:
+                            document.querySelector('#zycie0').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                            break; 
+                        case 1:
+                            document.querySelector('#zycie0').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                            break;
+                        case 0:
+                            document.querySelector('#zycie0').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                            strona = 1;
+                            wersy = 4;
+                            colorMaker();
+                            break;  
+                        }
+                } else {
+                    if (zyciaL === 2) {
+                        document.querySelector('#zycie0').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                        zyciaL = 0;
+                        document.querySelector('#results').innerHTML = '<h1>Niebieski przegrał</h1>'
+                    }
+                }
             } else {
                 zyciaP -= 1;
+                document.querySelector('#strona').innerHTML = strona;
+                if (rewanz === 0) {
+                    switch (zyciaP) {
+                        case 2:
+                            document.querySelector('#zycie1').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                            break; 
+                        case 1:
+                            document.querySelector('#zycie1').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                            break;
+                        case 0:
+                            document.querySelector('#zycie1').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                            strona = 0;
+                            wersy = 4;
+                            colorMaker();
+                            console.log('przegrales');
+                            break; 
+                    }
+                } else {
+                    if (zyciaP === 2) {
+                        document.querySelector('#zycie1').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
+                        zyciaP = 0;
+                        document.querySelector('#results').innerHTML = '<h1>Czerwony przegrał</h1>'
+                    }
+                }
             }
         }
 
-        if (strona === 0) {
-            document.querySelector('#tlo').style = 'background: #6CC5FF;';
-            strona = 1;
-            document.querySelector('#strona').innerHTML = strona;
-            switch (zyciaL) {
-                case 2:
-                    document.querySelector('#zycie0').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
-                    break; 
-                case 1:
-                    document.querySelector('#zycie0').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
-                    break;
-                case 0:
-                    document.querySelector('#zycie0').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
-                    console.log('przegrales');
-                    document.querySelector('#results').innerHTML += '<h1>Niebieski przegrał</h1>'
-                    break; 
-            }
-        } else {
-            document.querySelector('#tlo').style = 'background: #FF6C6C;';
-            strona = 0;
-            document.querySelector('#strona').innerHTML = strona;
-            switch (zyciaP) {
-                case 2:
-                    document.querySelector('#zycie1').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
-                    break; 
-                case 1:
-                    document.querySelector('#zycie1').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
-                    break;
-                case 0:
-                    document.querySelector('#zycie1').innerHTML += '<span style="font-size: 8rem; font-weight: extrabold;">X</span>';
-                    console.log('przegrales');
-                    document.querySelector('#results').innerHTML += '<h1>Czerwony przegrał</h1>'
-                    break; 
-            }
-        }
+        console.log(`zyciaL ${zyciaL}`)
+        console.log(`zyciaP ${zyciaP}`)
+
+        document.querySelector('#wynik').value = null;
     }
-}
+}   
